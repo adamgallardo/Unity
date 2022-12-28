c67d5f650d155cd5205485e1b5bdbd5b44eda488
adamgallardo <adam.gallardo@estudiantat.upc.edu>
2022-12-28 15:49:28 +0100

    Test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,12 +18,6 @@ public class PlayerMovement : MonoBehaviour
         movementVector = new Vector3();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
